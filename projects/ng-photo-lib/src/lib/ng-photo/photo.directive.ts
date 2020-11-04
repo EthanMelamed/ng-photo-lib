d059af78691e76142c7648722eb01e4f8236db4a
Ethan Melamed <melamee@navcanada.ca>
2020-11-04 12:31:19 -0500

    add prod and debug build commands. handle fixed backgrounds:

--- projects/ng-photo-lib/src/lib/ng-photo/photo.directive.ts
+++ projects/ng-photo-lib/src/lib/ng-photo/photo.directive.ts
@@ -11,9 +11,10 @@ export class PhotoDirective implements OnChanges {
   @Input('libPhoto') photo: PhotoModel | string;
   constructor(public el: ElementRef, public renderer: Renderer2) {}
 
+  // Ensures that the component  continues to display properly when an input is passed in or changed
   ngOnChanges(): void  {
     if (this.photo) {
-      this.handleResize();
+      this.update();
       if (this.photo instanceof PhotoModel) {
         this.renderer.setStyle(this.el.nativeElement, 'backgroundImage', `url("${this.photo.url}")`);
         this.renderer.setStyle(this.el.nativeElement, 'backgroundPosition', this.photo.focusPosition);
@@ -23,26 +24,43 @@ export class PhotoDirective implements OnChanges {
     }
   }
 
+  // Determine if the element has a taller ratio than the image
   isElDimRatioTaller(el: ElementRef, img: HTMLImageElement): boolean {
     if (!el || !img) {
       throw new Error('Cannot calculate ratios of null objects');
     }
-    return (el.nativeElement.offsetHeight / el.nativeElement.offsetWidth) / (img.naturalHeight / img.naturalWidth) > 1;
+
+    // Extract the heights and widths of the img and its container from DOM
+    let containerHeight = el.nativeElement.offsetHeight;
+    let containerWidth = el.nativeElement.offsetWidth;
+    let imgHeight = img.naturalHeight;
+    let imgWidth = img.naturalWidth;
+    if(getComputedStyle(this.el.nativeElement).backgroundAttachment === "fixed") {
+        containerHeight = window.innerHeight;
+        containerWidth = window.innerWidth;
+    }
+
+    // Calculate and return a boolean that the container has a taller ratio than th img
+    return (containerHeight / containerWidth) / (imgHeight / imgWidth) > 1;
   }
 
-  updateBackgroundSizing(elIsTaller: boolean): void {
-    if (elIsTaller === null || elIsTaller === undefined) {
+  // Sets the style of the element to bind to the horizontal or vertical edges
+  updateBackgroundSizing(containerIsTaller: boolean): void {
+    if (containerIsTaller === null || containerIsTaller === undefined) {
       throw Error('Cannot update background sizing using a null or undefined value');
     }
-    if (elIsTaller) {
+
+    // Directly set style on the element
+    if (containerIsTaller) {
       this.renderer.setStyle(this.el.nativeElement, 'backgroundSize', 'auto 100%');
     } else {
       this.renderer.setStyle(this.el.nativeElement, 'backgroundSize', '100% auto');
     }
   }
 
+  // Preloads the image, then updates the component by setting the image size based on a calculation.
   @HostListener('window:resize', ['$event'])
-  handleResize(event = null): void {
+  update(event = null): void {
     const img = document.createElement('img');
     img.onload = () => {
       this.updateBackgroundSizing(this.isElDimRatioTaller(this.el, img));
